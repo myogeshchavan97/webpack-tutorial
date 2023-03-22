0602ded0424e0f60956f4e3f2d5f5ee42ebd03ca
myogeshchavan97 <myogeshchavan97@gmail.com>
2023-03-23 00:14:44 +0530

    webpack - part 1

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: 'react'
+    })
+  ],
+  mode: 'development',
+  devServer: {
+    port: 5000,
+    historyApiFallback: true
+  }
+};
